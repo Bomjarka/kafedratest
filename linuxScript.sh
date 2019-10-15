fbea30f4a70b1935b71b96c054f9d2208ee17e04
Alexander <sashaxmas@gmail.com>
2019-10-15 17:33:33 +0700

    Обновление структуры

---
+++ linuxScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kotlinc src/app.kt -include-runtime -dstringTest.jar
+java -jar stringTest.jar Hello great world
+java -jar stringTest.jar the quick brown fox jumps over the lazy dog
+java -jar stringTest.jar Software engeneering is very interesting!
+echo "the quick brown fox jumps over the lazy dog" | java -jar stringTest.jar
+echo "blah blah bla" | java -jar stringTest.jar
+echo "Software engeneering is very interesting!" | java -jar stringTest.jar
