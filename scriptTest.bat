20744526a8fc2456a2285d5f7a47bc300dce50ee
Alexander <sashaxmas@gmail.com>
2019-10-15 16:06:09 +0700

    Добавление скрипта, который компилирует jar, а так же прогонка тестов внутри

---
+++ scriptTest.bat
@@ -0,0 +1,8 @@
+kotlinc src/app.kt -include-runtime -d out/stringTest.jar
+java -jar out/stringTest.jar Hello great world
+java -jar out/stringTest.jar the quick brown fox jumps over the lazy dog
+java -jar out/stringTest.jar Software engeneering is very interesting!
+echo "the quick brown fox jumps over the lazy dog" | java -jar out/stringTest.jar
+echo "blah blah bla" | java -jar out/stringTest.jar
+echo "Software engeneering is very interesting!" | java -jar out/stringTest.jar
+pause
