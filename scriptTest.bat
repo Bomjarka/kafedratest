1fcc2d2d9041954849d1384d669c52d562bdaad5
Alexander <sashaxmas@gmail.com>
2019-10-15 17:20:15 +0700

    Обновление структуры

--- scriptTest.bat
+++
@@ -1,8 +0,0 @@
-kotlinc src/app.kt -include-runtime -d out/stringTest.jar
-java -jar out/stringTest.jar Hello great world
-java -jar out/stringTest.jar the quick brown fox jumps over the lazy dog
-java -jar out/stringTest.jar Software engeneering is very interesting!
-echo "the quick brown fox jumps over the lazy dog" | java -jar out/stringTest.jar
-echo "blah blah bla" | java -jar out/stringTest.jar
-echo "Software engineering is very interesting!" | java -jar out/stringTest.jar
-pause
