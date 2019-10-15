70a28ddf30872e1fc5770e9dff039caafc4f6c3d
Alexander <sashaxmas@gmail.com>
2019-10-15 17:27:53 +0700

    Обновление структуры

--- scriptTest.bat
+++ scriptTest.bat
@@ -1,8 +1,8 @@
-kotlinc src/app.kt -include-runtime -d out/stringTest.jar
-java -jar out/stringTest.jar Hello great world
-java -jar out/stringTest.jar the quick brown fox jumps over the lazy dog
-java -jar out/stringTest.jar Software engineering is very interesting!
-echo "the quick brown fox jumps over the lazy dog" | java -jar out/stringTest.jar
-echo "blah blah bla" | java -jar out/stringTest.jar
-echo "Software engineering is very interesting!" | java -jar out/stringTest.jar
+kotlinc src/app.kt -include-runtime -d stringTest.jar
+java -jar stringTest.jar Hello great world
+java -jar stringTest.jar the quick brown fox jumps over the lazy dog
+java -jar stringTest.jar Software engineering is very interesting!
+echo "the quick brown fox jumps over the lazy dog" | java -jar stringTest.jar
+echo "blah blah bla" | java -jar stringTest.jar
+echo "Software engineering is very interesting!" | java -jar stringTest.jar
 pause
