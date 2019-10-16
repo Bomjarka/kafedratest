d0f09eef7fa5d39a23a72b0d9f9b34b4445b9d2e
Alexander <sashaxmas@gmail.com>
2019-10-16 09:57:35 +0700

    Обновление структуры

--- linuxScript.sh
+++ linuxScript.sh
@@ -6,3 +6,4 @@ java -jar stringTest.jar Software engeneering is very interesting!
 echo "the quick brown fox jumps over the lazy dog" | java -jar stringTest.jar
 echo "blah blah bla" | java -jar stringTest.jar
 echo "Software engeneering is very interesting!" | java -jar stringTest.jar
+read
