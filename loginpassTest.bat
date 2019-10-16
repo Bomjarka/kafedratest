476d035d8e46df0f589357966eaa080d7b3b1161
Alexander <sashaxmas@gmail.com>
2019-10-16 17:03:11 +0700

    Метод вызова справки + тест на её вызов -h

--- loginpassTest.bat
+++ loginpassTest.bat
@@ -1,4 +1,6 @@
 @echo off
 kotlinc src/loginpass.kt -include-runtime -d loginpass.jar
 echo "Project has been compiled"
+echo.
+java -jar loginpass.jar -h
 pause
--- src/loginpass.kt
+++ src/loginpass.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello world")
+    if (args[0] == "-h") {
+        throwFAQ()
+    } else {
+        val admin = Users("admin", "admin")
+        println("${admin.login} ${admin.password}")
+    }
+
+
 }
+
+data class Users(val login: String, val password: String)
+
+fun throwFAQ() = println("Это справка по работе с программой")
+
