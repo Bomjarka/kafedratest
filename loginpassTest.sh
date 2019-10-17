f3d987785db700a8754ed35a85882793e008760c
Alexander <sashaxmas@gmail.com>
2019-10-17 17:33:12 +0700

    Небольшие правки loginpassTest.sh

--- loginpassTest.sh
+++ loginpassTest.sh
@@ -40,13 +40,13 @@ java -jar loginpass.jar -password user -login user1
 echo -e
 
 echo "Testing login: u$er1 password: user (5.5)"
-echo "Excepted: Special symbols found"
+echo "Excepted: login format incorrect"
 echo "Actual:"
 java -jar loginpass.jar -login u$er1 -password user
 echo -e
 
 echo "Testing login: @dmin password: admin (5.5)"
-echo "Excepted: Special symbols found"
+echo "Excepted: login format incorrect"
 echo "Actual:"
 java -jar loginpass.jar -login @dmin -password @dmin
 echo -e
