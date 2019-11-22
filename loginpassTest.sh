9d838e6d86a5eb31e58373fe9c67a78c8f69ab6a
sashaxamas@gmail.com <Xamas123>
2019-11-23 01:36:48 +0700

    Обновил loginpassTest.sh - теперь при прогонке тестов идёт проверка на количество ошибок

--- loginpassTest.sh
+++ loginpassTest.sh
@@ -1,114 +1,181 @@
 #!/bin/bash
+checkExitCode(){
+  if [ "$1" -ne "$2" ]; then
+  let fails=fails+1
+fi
+}
+printResult(){
+  if [ "$fails" -ne "$success" ]; then
+    echo "kek"
+  else
+    echo "All test have been passed successful"
+fi
+}
+
+
+classpath="lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar;loginpass.jar"
+fails=0
+success=0
 
 echo -e "Starting compilation"
-kotlinc -cp lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar src/com/chirkin/kafedratest  -include-runtime -d loginpass.jar
+kotlinc -cp lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar src -include-runtime -d loginpass.jar
 echo -e
 echo -e "Starting tests"
+
 
 echo "Testing no parameters (5.1)"
 echo "Excepted: Exit code 1 (print Reference)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt
+actual=$?
+expected=1
+checkExitCode $actual $expected
 echo -e
-
 
 echo "Testing -h parameter (5.2)"
 echo "Excepted: Exit code 1 (print Reference)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt -h
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt -h
 echo -e
 
 echo "Testing login: Admin password: admin (5.3)"
 echo "Excepted: Exit code 0 (Login successful)"
 echo "Actual:"
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login Admin --password admin
+checkExitCode $actual $expected
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login Admin --password admin
+actual=$?
+expected=0
+checkExitCode $actual $expected
 echo -e
-
-
 
 echo "Testing login: User1 password: user (5.3)"
 echo "Excepted: Exit code 0 (Login successful)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login User1 --password user
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login User1 --password user
+actual=$?
+expected=0
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing password: admin login: Admin (5.4)"
 echo "Excepted: Exit code 0 (Login successful)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --password admin --login Admin
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --password admin --login Admin
+actual=$?
+expected=0
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing password: ser login: User1 (5.4)"
 echo "Excepted: Exit code 0 (Login successful)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --password user --login User1
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --password user --login User1
+actual=$?
+expected=0
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing login: u\$er1 password: user (5.5)"
 echo "Excepted: Exit code 2 (Login format incorrect)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login u$er1 --password user
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login u\$er1 --password user
+actual=$?
+expected=2
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing login: @dmin password: admin (5.5)"
 echo "Excepted: Exit code 2 (Login format incorrect)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login @dmin --password @dmin
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login @dmin --password @dmin
+actual=$?
+expected=2
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing login: User242 password: user (5.6)"
 echo "Excepted: Exit code 3 (User not found)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login User242 --password user
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login User242 --password user
+actual=$?
+expected=3
+checkExitCode $actual $expected
 echo -e
 
 
 echo "Testing login: Admin password: 1234 (5.7)"
 echo "Excepted: Exit code 4 (Password incorrect)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login Admin --password 1234
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login Admin --password 1234
+actual=$?
+expected=4
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing login: Admin"
 echo "Excepted: Exit code 1"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login Admin
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login Admin
+actual=$?
+expected=1
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing blablablha"
 echo "Excepted: Exit code 1"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login Admin
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login Admin
+actual=$?
+expected=1
+checkExitCode $actual $expected
 echo -e
 
 
 echo "Testing login: Admin password: admin role: EXECUTE resource: A.BC (5.8)"
 echo "Excepted: Exit code 0 (Login successful)"
 echo "Actual: "
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login Admin --password admin --role EXECUTE --resource A.BC
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login Admin --password admin --role EXECUTE --resource A.BC
+actual=$?
+expected=0
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing login: User1 password: user role: READ resource: A.BC.D.E (5.8)"
 echo "Excepted: Exit code 0 (Login successful)"
 echo "Actual":
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login User1 --password user --role READ --resource A.BC.D.E
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login User1 --password user --role READ --resource A.BC.D.E
+actual=$?
+expected=0
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing  password: admin role: EXECUTE login: Admin resource: A.BC (5.9)"
 echo "Excepted: Exit code 0 (Login successful)"
 echo "Actual":
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --password admin --role EXECUTE --login Admin  --resource A.BC
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --password admin --role EXECUTE --login Admin  --resource A.BC
+actual=$?
+expected=0
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing login: Admin password: admin role: DELETE resource: A.BC (5.10)"
 echo "Excepted: Exit code 5 (Role unknown)"
 echo "Actual":
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login Admin --password admin --role DELETE --resource A.BC
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login Admin --password admin --role DELETE --resource A.BC
+actual=$?
+expected=5
+checkExitCode $actual $expected
 echo -e
 
 echo "Testing login: Admin password: admin role: EXECUTE resource: A.BC.EF (5.10)"
 echo "Excepted: Exit code 6 (Success do this resource is denied)"
 echo "Actual":
-java -cp "lib/kotlinx-cli-jvm-0.2.0-SNAPSHOT.jar:loginpass.jar" com.chirkin.kafedratest.LoginpassKt --login Admin --password admin --role EXECUTE --resource A.BC.EF
+java -cp $classpath com.chirkin.kafedratest.LoginpassKt --login Admin --password admin --role EXECUTE --resource A.BC.EF
+actual=$?
+expected=6
+checkExitCode $actual $expected
 echo -e
+
+printResult
+
 read
