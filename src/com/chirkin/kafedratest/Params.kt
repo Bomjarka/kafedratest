f645a810d3c07734dfa7d9293b0a0bb9472d25be
sashaxamas@gmail.com <Xamas123>
2019-11-25 23:15:07 +0700

    1 - добавил новые параметры в класс Params

--- src/com/chirkin/kafedratest/Params.kt
+++ src/com/chirkin/kafedratest/Params.kt
@@ -12,12 +12,18 @@ class Params(args: Array<String>) {
     val resource: String
     val isHelp: Boolean
     val isAuth: Boolean
+    val dateStart: String
+    val dateEnd: String
+    val volume: Int
 
     private val parser = ArgParser("Input string")
     private val uLogin by parser.option(ArgType.String, fullName = "login", shortName = "log", description = "User login")
     private val uPassword by parser.option(ArgType.String, fullName = "password", shortName = "pwd", description = "user password")
     private val uRole by parser.option(ArgType.String, fullName = "role", shortName = "ro", description = "User role")
     private val uResource by parser.option(ArgType.String, fullName = "resource", shortName = "rs", description = "Resource")
+    private val udateStart by parser.option(ArgType.String, fullName = "dateStart", shortName = "ds", description = "Date start")
+    private val udateEnd by parser.option(ArgType.String, fullName = "dateEnd", shortName = "de", description = "Date end")
+    private val uvolume by parser.option(ArgType.String, fullName = "volume", shortName = "vol", description = "Volume")
 
     init {
         parser.parse(args)
@@ -28,6 +34,9 @@ class Params(args: Array<String>) {
             password = ""
             role = ""
             resource = ""
+            dateStart = ""
+            dateEnd = ""
+            volume = 0
         } else if (args.size == 4 && uLogin != null && uPassword != null) {
             isHelp = false
             isAuth = false
@@ -35,11 +44,17 @@ class Params(args: Array<String>) {
             password = uPassword!!
             role = ""
             resource = ""
+            dateStart = ""
+            dateEnd = ""
+            volume = 0
         } else if (args.size == 8) {
             login = uLogin!!
             password = uPassword!!
             role = uRole!!
             resource = uResource!!
+            dateStart = udateStart!!
+            dateEnd = udateEnd!!
+            volume = 0
             isHelp = false
             isAuth = true
         } else {
@@ -49,6 +64,9 @@ class Params(args: Array<String>) {
             password = ""
             role = ""
             resource = ""
+            dateStart = ""
+            dateEnd = ""
+            volume = 0
         }
     }
 }
